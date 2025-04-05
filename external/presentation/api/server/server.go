f88bde8ed01de15f35c3abca086e27a31cabbec1
amir <amir1378khalili@gmail.com>
2025-04-05 12:30:07 +0330

    ADD new configs part

--- external/presentation/api/server/server.go
+++ external/presentation/api/server/server.go
@@ -6,6 +6,7 @@ import (
 	"hub_logging/config"
 	"hub_logging/external/infra/di"
 	"hub_logging/external/presentation/api/controller"
+	"hub_logging/external/presentation/api/middleware"
 	"hub_logging/external/presentation/api/routes/rest"
 
 	"github.com/gofiber/fiber/v2"
@@ -15,11 +16,15 @@ import (
 // and starts the HTTP server.
 func StartServer(cfg config.AppConfig) {
 	// Initialize a new Fiber instance.
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		ErrorHandler: middleware.ErrorMiddleware,
+	})
 
-	// Set up your dependency injection container.
-	container := di.NewContainer(cfg)
-
+	// Initialize the container with dependencies.
+	container, err := di.InitializeContainer(cfg)
+	if err != nil {
+		log.Fatalf("Failed to initialize container: %v", err)
+	}
 	// Create an instance of LogController using the dependencies provided by the container.
 	logController := controller.NewLogController(container.CreateLogUseCase, container.LogMessageRepo)
 
