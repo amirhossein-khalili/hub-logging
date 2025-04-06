fb62085d48e8736c04ff4e819cab56415734a012
amir <amir1378khalili@gmail.com>
2025-04-06 11:47:04 +0330

    ADD sarma packages and remove comments

--- pkg/infrastructure/messaging/kafka/consumer.go
+++ pkg/infrastructure/messaging/kafka/consumer.go
@@ -1,54 +1,54 @@
 package kafka
 
-// import (
-// 	"encoding/json"
-// 	"hub_logging/pkg/application"
+import (
+	"encoding/json"
+	"hub_logging/pkg/application"
 
-// 	"github.com/IBM/sarama"
-// )
+	"github.com/IBM/sarama"
+)
 
-// type KafkaConsumer struct {
-// 	consumer sarama.Consumer
-// 	handlers map[string]func(application.DomainEvent)
-// }
+type KafkaConsumer struct {
+	consumer sarama.Consumer
+	handlers map[string]func(application.DomainEvent)
+}
 
-// func NewKafkaConsumer(brokers []string) (*KafkaConsumer, error) {
-// 	config := sarama.NewConfig()
-// 	consumer, err := sarama.NewConsumer(brokers, config)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return &KafkaConsumer{
-// 		consumer: consumer,
-// 		handlers: make(map[string]func(application.DomainEvent)),
-// 	}, nil
-// }
+func NewKafkaConsumer(brokers []string) (*KafkaConsumer, error) {
+	config := sarama.NewConfig()
+	consumer, err := sarama.NewConsumer(brokers, config)
+	if err != nil {
+		return nil, err
+	}
+	return &KafkaConsumer{
+		consumer: consumer,
+		handlers: make(map[string]func(application.DomainEvent)),
+	}, nil
+}
 
-// func (c *KafkaConsumer) Subscribe(topic string, handler func(event application.DomainEvent)) error {
-// 	c.handlers[topic] = handler
-// 	return nil
-// }
+func (c *KafkaConsumer) Subscribe(topic string, handler func(event application.DomainEvent)) error {
+	c.handlers[topic] = handler
+	return nil
+}
 
-// func (c *KafkaConsumer) Start() error {
-// 	for topic := range c.handlers {
-// 		partitionConsumer, err := c.consumer.ConsumePartition(topic, 0, sarama.OffsetNewest)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		go func(pc sarama.PartitionConsumer) {
-// 			for msg := range pc.Messages() {
-// 				var event application.DomainEvent
-// 				if err := json.Unmarshal(msg.Value, &event); err == nil {
-// 					if handler, ok := c.handlers[topic]; ok {
-// 						handler(event)
-// 					}
-// 				}
-// 			}
-// 		}(partitionConsumer)
-// 	}
-// 	return nil
-// }
+func (c *KafkaConsumer) Start() error {
+	for topic := range c.handlers {
+		partitionConsumer, err := c.consumer.ConsumePartition(topic, 0, sarama.OffsetNewest)
+		if err != nil {
+			return err
+		}
+		go func(pc sarama.PartitionConsumer) {
+			for msg := range pc.Messages() {
+				var event application.DomainEvent
+				if err := json.Unmarshal(msg.Value, &event); err == nil {
+					if handler, ok := c.handlers[topic]; ok {
+						handler(event)
+					}
+				}
+			}
+		}(partitionConsumer)
+	}
+	return nil
+}
 
-// func (c *KafkaConsumer) Close() error {
-// 	return c.consumer.Close()
-// }
+func (c *KafkaConsumer) Close() error {
+	return c.consumer.Close()
+}
--- pkg/infrastructure/messaging/kafka/publisher.go
+++ pkg/infrastructure/messaging/kafka/publisher.go
@@ -1,45 +1,45 @@
 package kafka
 
-// import (
-// 	"encoding/json"
-// 	"hub_logging/pkg/application"
+import (
+	"encoding/json"
+	"hub_logging/pkg/application"
 
-// 	"github.com/IBM/sarama"
-// )
+	"github.com/IBM/sarama"
+)
 
-// type KafkaPublisher struct {
-// 	producer sarama.SyncProducer
-// }
+type KafkaPublisher struct {
+	producer sarama.SyncProducer
+}
 
-// func NewKafkaPublisher(brokers []string) (*KafkaPublisher, error) {
-// 	config := sarama.NewConfig()
-// 	config.Producer.RequiredAcks = sarama.WaitForAll // Wait for all replicas to acknowledge
-// 	config.Producer.Retry.Max = 5                    // Retry up to 5 times
-// 	config.Producer.Return.Successes = true          // Ensure we get confirmation of success
+func NewKafkaPublisher(brokers []string) (*KafkaPublisher, error) {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll // Wait for all replicas to acknowledge
+	config.Producer.Retry.Max = 5                    // Retry up to 5 times
+	config.Producer.Return.Successes = true          // Ensure we get confirmation of success
 
-// 	producer, err := sarama.NewSyncProducer(brokers, config)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return &KafkaPublisher{producer: producer}, nil
-// }
+	producer, err := sarama.NewSyncProducer(brokers, config)
+	if err != nil {
+		return nil, err
+	}
+	return &KafkaPublisher{producer: producer}, nil
+}
 
-// func (p *KafkaPublisher) Publish(event application.DomainEvent) error {
-// 	// Serialize event to JSON
-// 	data, err := json.Marshal(event)
-// 	if err != nil {
-// 		return err
-// 	}
+func (p *KafkaPublisher) Publish(event application.DomainEvent) error {
+	// Serialize event to JSON
+	data, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
 
-// 	// Publish to Kafka
-// 	topic := "events" // You can make this configurable
-// 	_, _, err = p.producer.SendMessage(&sarama.ProducerMessage{
-// 		Topic: topic,
-// 		Value: sarama.ByteEncoder(data),
-// 	})
-// 	return err
-// }
+	// Publish to Kafka
+	topic := "events" // You can make this configurable
+	_, _, err = p.producer.SendMessage(&sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.ByteEncoder(data),
+	})
+	return err
+}
 
-// func (p *KafkaPublisher) Close() error {
-// 	return p.producer.Close()
-// }
+func (p *KafkaPublisher) Close() error {
+	return p.producer.Close()
+}
